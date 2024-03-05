6054bc17ead3f16ae9e02103dd52ebbe8e01c892
Avidev Jha <avidevjha9@gmail.com>
2024-03-05 20:50:20 +0530

    add category Added

--- src/components/pages/product/Products.jsx
+++ src/components/pages/product/Products.jsx
@@ -3,7 +3,6 @@ import { usefilterContext } from '../../../context'
 import { fetchProducts, fetchAllProducts } from '../../../api/Api'
 import { Link } from 'react-router-dom'
 const Products = ({ filterItem }) => {
-  console.log(filterItem)
   const [product, setProducts] = useState([])
   const { changeFilter, filteredItem } = usefilterContext()
   useEffect(() => {
@@ -15,7 +14,6 @@ const Products = ({ filterItem }) => {
           setProducts(res.product)
         })
   }, [filterItem])
-  console.log(product)
   return (
     <div className='w-[calc(100%_-_200px)] flex flex-row flex-wrap justify-start items-start content-start gap-2 bg-[#fff] px-2'>
       <div className='w-[100%] border rounded-md p-2 flex justify-end  gap-y-2 mb-2'>
