a029460edcfea151de90d07818a525c1b2768720
Rudhiranjan2001 <silicon.rudhi563@gmail.com>
2024-03-11 19:52:00 +0530

    order page

--- src/components/layouts/SideBar.jsx
+++ src/components/layouts/SideBar.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const SideBar = () => {
+    const clickedpath={
+        
+    }
     return (
         <div className='w-[200px] flex flex-col bg-[#dedede] border-r-2 p-2 pl-[24px] gap-2 font-medium'>
-            <Link to="/" className='w-full flex flex-row justify-start items-center gap-2 cursor-pointer'>
+            <Link to="/" className='w-full flex flex-row justify-start items-center gap-2 cursor-pointer hover:bg-[#f6f6f6]'>
                 <div>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#747475" className="bi bi-microsoft" viewBox="0 0 16 16">
                         <path d="M7.462 0H0v7.19h7.462zM16 0H8.538v7.19H16zM7.462 8.211H0V16h7.462zm8.538 0H8.538V16H16z" />
@@ -17,9 +20,9 @@ const SideBar = () => {
                 </div>
             </Link>
             <div className='w-full flex flex-row justify-start items-center gap-2'>
-                <div className="collapse py-2">
+                <div className="collapse ">
                     <input type="checkbox" className='min-h-0' />
-                    <div className="flex flex-row collapse-title min-h-0 p-0 gap-3 py-1">
+                    <div className="flex flex-row collapse-title min-h-0 p-0 gap-3 py-2 px-1 hover:bg-[#f6f6f6] rounded-md">
                         <div className='w-auto '>
                             <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className="bi bi-currency-rupee" viewBox="0 0 16 16">
                                 <path d="M4 3.06h2.726c1.22 0 2.12.575 2.325 1.724H4v1.051h5.051C8.855 7.001 8 7.558 6.788 7.558H4v1.317L8.437 14h2.11L6.095 8.884h.855c2.316-.018 3.465-1.476 3.688-3.049H12V4.784h-1.345c-.08-.778-.357-1.335-.793-1.732H12V2H4z" />
@@ -28,7 +31,7 @@ const SideBar = () => {
                         Sales
                     </div>
                     <div className="collapse-content flex flex-col gap-2 py-0">
-                        <Link to="/commands" className='flex flex-row gap-2 cursor-pointer pt-3'>
+                        <Link to="/commands" className='flex flex-row gap-2 cursor-pointer py-2 px-1 hover:bg-[#f6f6f6] rounded-md'>
                             <span>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-stickies" viewBox="0 0 16 16">
                                     <path d="M1.5 0A1.5 1.5 0 0 0 0 1.5V13a1 1 0 0 0 1 1V1.5a.5.5 0 0 1 .5-.5H14a1 1 0 0 0-1-1z" />
@@ -37,7 +40,7 @@ const SideBar = () => {
                             </span>
                             Orders
                         </Link>
-                        <Link to={'/category'} className='flex flex-row gap-2 cursor-pointer'>
+                        <Link to={'/category'} className='flex flex-row gap-2 cursor-pointer py-2 px-1 hover:bg-[#f6f6f6] rounded-md'>
                             <span>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-bookmarks" viewBox="0 0 16 16">
                                     <path d="M2 4a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v11.5a.5.5 0 0 1-.777.416L7 13.101l-4.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v10.566l3.723-2.482a.5.5 0 0 1 .554 0L11 14.566V4a1 1 0 0 0-1-1z" />
@@ -49,26 +52,11 @@ const SideBar = () => {
                     </div>
                 </div>
             </div>
-            {/* <div className='w-full flex flex-row justify-start items-center gap-2'>
-                <div className="collapse py-2">
-                    <input type="checkbox" className='min-h-0' />
-                    <div className="flex flex-row collapse-title min-h-0 p-0 gap-2">
-                        <div className='w-auto '>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className="bi bi-currency-rupee" viewBox="0 0 16 16">
-                                <path d="M4 3.06h2.726c1.22 0 2.12.575 2.325 1.724H4v1.051h5.051C8.855 7.001 8 7.558 6.788 7.558H4v1.317L8.437 14h2.11L6.095 8.884h.855c2.316-.018 3.465-1.476 3.688-3.049H12V4.784h-1.345c-.08-.778-.357-1.335-.793-1.732H12V2H4z" />
-                            </svg>
-                        </div>
-                        Sales
-                    </div>
-                    <div className="collapse-content">
-                        <p>hello</p>
-                    </div>
-                </div>
-            </div> */}
+            
             <div className='w-full flex flex-row justify-start items-center gap-2'>
-                <div className="collapse py-2">
+                <div className="collapse ">
                     <input type="checkbox" className='min-h-0' />
-                    <div className="flex flex-row collapse-title min-h-0 p-0 gap-3 py-1">
+                    <div className="flex flex-row collapse-title min-h-0 p-0 gap-3 py-2 px-1  hover:bg-[#f6f6f6] rounded-md">
                         <div className='w-auto '>
                             <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className="bi bi-bookmark-plus" viewBox="0 0 16 16">
                                 <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z" />
@@ -78,7 +66,7 @@ const SideBar = () => {
                         Catelogs
                     </div>
                     <div className="collapse-content flex flex-col gap-2 py-0">
-                        <Link to="/products" className='flex flex-row gap-2 cursor-pointer pt-3'>
+                        <Link to="/products" className='flex flex-row gap-2 cursor-pointer py-2 px-1 hover:bg-[#f6f6f6] rounded-md'>
                             <span>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-stickies" viewBox="0 0 16 16">
                                     <path d="M1.5 0A1.5 1.5 0 0 0 0 1.5V13a1 1 0 0 0 1 1V1.5a.5.5 0 0 1 .5-.5H14a1 1 0 0 0-1-1z" />
@@ -87,7 +75,7 @@ const SideBar = () => {
                             </span>
                             Products
                         </Link>
-                        <Link to={'/category'} className='flex flex-row gap-2 cursor-pointer'>
+                        <Link to={'/category'} className='flex flex-row gap-2 cursor-pointer py-2 px-1 hover:bg-[#f6f6f6] rounded-md'>
                             <span>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-bookmarks" viewBox="0 0 16 16">
                                     <path d="M2 4a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v11.5a.5.5 0 0 1-.777.416L7 13.101l-4.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v10.566l3.723-2.482a.5.5 0 0 1 .554 0L11 14.566V4a1 1 0 0 0-1-1z" />
